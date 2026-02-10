3b76cbffe8991231199953a73961f172d3ee1ee2
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-10 23:47:06 +0900

    Remove NOTION_PAGE_ID from next.config.js
    Removed NOTION_PAGE_ID from environment variables.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  env: {
-    NOTION_PAGE_ID: '30395356c351806cacfce22fcbf53236',
-  },
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
