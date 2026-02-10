695bb2371c4d6f1b610a5f3d6bc975f4a7f2aff3
Daebok <138689677+YDaewon@users.noreply.github.com>
2026-02-10 23:42:25 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    NOTION_PAGE_ID: '30395356c351806cacfce22fcbf53236',
+  },
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
